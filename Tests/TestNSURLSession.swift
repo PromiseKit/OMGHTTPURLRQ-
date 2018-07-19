0251dff4ced9c6b05a63d9cc1055cb8e3d6e7fc8
Doug <dougstein@gmail.com>
2018-07-18 17:30:03 -0700

    'Cancel' for PromiseKit

--- Tests/TestNSURLSession.swift
+++ Tests/TestNSURLSession.swift
@@ -72,3 +72,81 @@ class NSURLSessionTests: XCTestCase {
         OHHTTPStubs.removeAllStubs()
     }
 }
+
+//////////////////////////////////////////////////////////// Cancellation
+
+extension NSURLSessionTests {
+    func testCancel1() {
+        let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
+
+        OHHTTPStubs.stubRequests(passingTest: { $0.url!.host == "example.com" }) { _ in
+            return OHHTTPStubsResponse(jsonObject: json, statusCode: 200, headers: nil)
+        }
+
+        let ex = expectation(description: "")
+        URLSession.shared.GETCC("http://example.com").compactMap {
+            XCTFail()
+            try JSONSerialization.jsonObject(with: $0.data)
+        }.done {
+            XCTAssertEqual(json, $0 as? NSDictionary)
+            XCTFail()
+        }.catch(policy: .allErrors) {
+            $0.isCancelled ? ex.fulfill() : XCTFail()
+        }.cancel()
+        waitForExpectations(timeout: 1)
+    }
+
+    func testCancel2() {
+
+        // test that Promise<Data> chains thens
+        // this test because I don’t trust the Swift compiler
+
+        let dummy = ("fred" as NSString).data(using: String.Encoding.utf8.rawValue)!
+
+        OHHTTPStubs.stubRequests(passingTest: { $0.url!.host == "example.com" }) { _ in
+            return OHHTTPStubsResponse(data: dummy, statusCode: 200, headers: [:])
+        }
+
+        let ex = expectation(description: "")
+
+        afterCC(seconds: 0.1).then { () -> CancellablePromise<(data: Data, response: URLResponse)> in
+            let p = URLSession.shared.GETCC("http://example.com")
+            p.cancel()
+            return p
+        }.done {
+            XCTAssertEqual($0.data, dummy)
+            XCTFail()
+        }.catch(policy: .allErrors) {
+            $0.isCancelled ? ex.fulfill() : XCTFail()
+        }
+
+        waitForExpectations(timeout: 1)
+    }
+
+    func testCancelSyntax() {
+        let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
+
+        OHHTTPStubs.stubRequests(passingTest: {
+            $0.url!.host == "example.com"
+        }, withStubResponse: { _ in
+            OHHTTPStubsResponse(jsonObject: json, statusCode: 200, headers: nil)
+        })
+
+        let p = URLSession.shared.GETCC("http://example.com", query: [
+            "1": 1,
+            "2": 2
+        ])
+
+        let ex = expectation(description: "")
+        p.compactMap {
+            p.cancel()
+            try JSONSerialization.jsonObject(with: $0.data)
+        }.done {
+            XCTAssertEqual(json, $0 as? NSDictionary)
+            XCTFail()
+        }.catch(policy: .allErrors) {
+            $0.isCancelled ? ex.fulfill() : XCTFail()
+        }
+        waitForExpectations(timeout: 1)
+    }
+}
